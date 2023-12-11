7d118c6832dfee81b1a5d3294df3acaa63272543
Slenderslayer88 <Slenderslayer88@gmail.com>
2023-12-11 13:55:26 -0700

    Added footer

--- Vue-Notes-App/src/App.vue
+++ Vue-Notes-App/src/App.vue
@@ -9,6 +9,15 @@ const num = 10;
 <template>
 	<div id="top"></div><!--Teleport target-->
 	<NotesContainer /> <!--Component-->
+    <footer class="mb-5 ml-5 fixed bottom-0 left-0">
+        <div id="Rory's contributions" >
+            Garret Mullings's contributions: Testing
+        </div>
+        <div id="Rory's contributions" >
+            Rory Schillo's contributions: My main contribution was adding the note editor popup modal. This feature allows users to edit the main text and title of notes using Vue's modal 
+            system. I also did most of the styling for the app and created the presentation slides. And I assisted in the making of the Note and NoteCollection components.
+        </div>
+    </footer>
 </template>
 
 <style>
